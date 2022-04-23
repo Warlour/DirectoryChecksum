ba8b74b9a5c77eca93291fb16777d0d1ee90c909
Warlour <warlour@eksworld.dk>
2022-04-23 20:21:15 +0200

    Trying to add size compare

--- directorycompare.py
+++ directorycompare.py
@@ -34,6 +34,7 @@ def find_duplicates(files, duplicates = {}):
             duplicates[hash].add(file)
         else:
             duplicates[hash] = {file}
+        print(duplicates)
 
     nonduplicates = []
 
@@ -47,15 +48,8 @@ def find_duplicates(files, duplicates = {}):
 
     return duplicates
 
-def comparechecksums():
-    files = [] # All file names
-    hashes = [] # SHA256-hash of all file names
-    duplicates = {} # Duplicate files
-    txtprint.pack_forget()
-    listbox.delete(0, tk.END)
-    listbox.pack_forget()
-    label.pack_forget()
-
+def getfiles():
+    files = []
     path = pathbox.get("1.0", 'end-1c')
     if not os.path.exists(os.path.dirname(path)) or not os.path.isdir(path):
         timerpackunpack(label, "Not a path to a directory")
@@ -66,24 +60,83 @@ def comparechecksums():
                 # Check if it is a file
                 if os.path.isfile(f) and not os.path.isdir(f):
                     files.append(f)
-        if files:
-            duplicates = find_duplicates(files, duplicates)
+    return files
+
+def comparechecksums():
+    duplicates = {} # Duplicate files
+    txtprint.pack_forget()
+    listbox.delete(0, tk.END)
+    listbox.pack_forget()
+    label.pack_forget()
+
+    files = getfiles()
+    if files:
+        duplicates = find_duplicates(files, duplicates)
+        
+        idx = 1
+        for duplicateset in duplicates.values():
+            listbox.insert(idx, "These files share the same checksum: ")
+            idx += 1
+            for element in duplicateset:
+                listbox.insert(idx, element)
+                idx += 1
+            listbox.insert(idx, "")
+            idx += 1
+        label.configure(text = "List of files with the same checksum")
+        label.pack()
+        listbox.pack()
+        txtprint.pack()
+    else:
+        timerpackunpack(label, "No duplicates found within the directory")
+
+def comparesizes():
+    duplicates = {}
+    files = getfiles()
+
+    txtprint.pack_forget()
+    listbox.delete(0, tk.END)
+    listbox.pack_forget()
+    label.pack_forget()
+
+    if files:
+        for file in files:
+            size = os.path.getsize(file)
+
+            # Add this hash occurrence to the `duplicates` dict. It will be
+            # removed later if no other duplicates are found.
+            if size in duplicates:
+                duplicates[size].add(file)
+            else:
+                duplicates[size] = {file}
+            print(duplicates)
+
+            nonduplicates = []
+
+            # Remove non-duplicates from 
+            for key in duplicates:
+                if len(duplicates[key]) < 2:
+                    nonduplicates.append(key)
             
-            idx = 1
-            for duplicateset in duplicates.values():
-                listbox.insert(idx, "These files share the same checksum: ")
+            for key in nonduplicates:
+                del duplicates[key]
+
+        idx = 1
+        for duplicateset in duplicates.values():
+            listbox.insert(idx, "These files share the same size: ")
+            idx += 1
+            for element in duplicateset:
+                listbox.insert(idx, element)
                 idx += 1
-                for element in duplicateset:
-                    listbox.insert(idx, element)
-                    idx += 1
-                listbox.insert(idx, "")
-                idx += 1
-            label.configure(text = "List of files with the same checksum")
-            label.pack()
-            listbox.pack()
-            txtprint.pack()
-        else:
-            timerpackunpack(label, "No duplicates found within the directory")
+            listbox.insert(idx, "")
+            idx += 1
+        label.configure(text = "List of files with the same size")
+        label.pack()
+        listbox.pack()
+        txtprint.pack()
+    else:
+        timerpackunpack(label, "No duplicates found within the directory")
+
+                    
 
 def tofile():
     if listbox.get(first=0, last=tk.END):
@@ -97,7 +150,7 @@ window = tk.Tk()
 window.title("Directory compare")
 window.geometry("750x800")
 window.configure(bg = "#F2F2F2")
-pathlabel = tk.Label(window, text="Path to compare checksum", justify = "left")
+pathlabel = tk.Label(window, text="Path to compare directories", justify = "left")
 pathbox = tk.Text(window, height = 2, width = window.winfo_reqwidth(), bg = "#CDCDCD")
 listbox = tk.Listbox(window, width = window.winfo_reqwidth(), height = window.winfo_reqheight()-500)
 txtprint = tk.Button(window, text="Print to txt-file", width = 15, command=tofile)
@@ -106,7 +159,9 @@ label = tk.Label(window, text="", justify="center")
 pathlabel.pack()
 pathbox.pack()
 
-calc = tk.Button(window, text="Get checksum", command = comparechecksums)
-calc.pack()
+checksumget = tk.Button(window, text="Get checksum", command = comparechecksums)
+sizeget = tk.Button(window, text="Get size", command=comparesizes)
+checksumget.pack()
+sizeget.pack()
 
 window.mainloop()
